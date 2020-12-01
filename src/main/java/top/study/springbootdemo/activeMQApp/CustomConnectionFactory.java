160113fefd832c30d9799cb588d2822202975bc0
jjhe <jjhe@travelsky.com>
2020-12-01 14:20:45 +0800

    修改为子项目

--- src/main/java/top/study/springbootdemo/activeMQApp/CustomConnectionFactory.java
+++
@@ -1,6 +0,0 @@
-package top.study.springbootdemo.activeMQApp;
-
-public class CustomConnectionFactory {
-
-
-}
--- src/main/java/top/study/springbootdemo/mybatisplus/entity/vo/TeacherStudent.java
+++
@@ -1,9 +0,0 @@
-package top.study.springbootdemo.mybatisplus.entity.vo;
-
-import lombok.Data;
-
-@Data
-public class TeacherStudent {
-
-
-}
